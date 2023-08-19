8216caf0ec73857b188110ba72a5cb5ac2f31070
Alex Barber <alex@dangerzonegames.com>
2023-08-18 18:53:08 -0600

    a bit of refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! An Entity Component System Library
+use std::array::from_fn;
+use std::cmp::max;
 use std::fmt::Debug;
-use std::iter::repeat;
+use std::iter::repeat_with;
 
 pub trait Component: Sized + Debug {}
+
+pub enum ComponentStorageError {}
 
 pub trait ComponentStorage<T: Component> {
     fn get_component(&self, index: usize) -> Option<&'_ T>;
@@ -10,33 +14,46 @@ pub trait ComponentStorage<T: Component> {
     fn delete_component(&mut self, index: usize) -> Result<(), ()>;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ArrayComponentStorage<T: Component, const SIZE: usize> {
     components: [Option<T>; SIZE],
 }
 
-impl<T, const SIZE: usize> ComponentStorage<T> for ArrayComponentStorage<T, SIZE> where T: Component {
+impl<T, const SIZE: usize> ComponentStorage<T> for ArrayComponentStorage<T, SIZE>
+where
+    T: Component,
+{
     fn get_component(&self, index: usize) -> Option<&'_ T> {
-        self.components.get(index)
+        if let Some(Some(component)) = self.components.get(index) {
+            return Some(component);
+        }
+        None
     }
 
     fn set_component(&mut self, index: usize, component: T) -> Result<(), ()> {
-        match index {
-            index if index < SIZE => {
-                self.components.get_mut(index) = Some(component);
-                Ok(())
-            }
-            _ => Err(())
+        if let Some(stored) = self.components.get_mut(index) {
+            *stored = Some(component);
+            return Ok(());
         }
+        Err(())
     }
 
     fn delete_component(&mut self, index: usize) -> Result<(), ()> {
-        match index {
-            index if index < SIZE => {
-                self.components.get_mut(index) = None;
-                Ok(())
-            }
-            _ => Err(())
+        if let Some(stored) = self.components.get_mut(index) {
+            *stored = None;
+            return Ok(());
+        }
+        Err(())
+    }
+}
+
+impl<T, const SIZE: usize> Default for ArrayComponentStorage<T, SIZE>
+where
+    T: Component,
+{
+    fn default() -> Self {
+        Self {
+            components: from_fn(|_| None),
         }
     }
 }
@@ -46,33 +63,51 @@ pub struct VecComponentStorage<T: Component> {
     components: Vec<Option<T>>,
 }
 
-impl<T> VecComponentStorage<T> where T: Component {
+impl<T> VecComponentStorage<T>
+where
+    T: Component,
+{
     pub fn new_with_initial_size(initial_size: usize) -> Self {
         let mut components = vec![];
         components.reserve(initial_size);
-        Self {
-            components
-        }
+        Self { components }
     }
 }
 
-impl<T> ComponentStorage<T> for VecComponentStorage<T> where T: Component {
+impl<T> VecComponentStorage<T>
+where
+    T: Component,
+{
+    fn resize(&mut self, min_size: usize) -> Result<(), ()> {
+        let additional = max(0, min_size - self.components.len());
+        if additional > 0 {
+            self.components.reserve(additional);
+            self.components
+                .extend(repeat_with(|| None).take(additional));
+        }
+        Ok(())
+    }
+}
+
+impl<T> ComponentStorage<T> for VecComponentStorage<T>
+where
+    T: Component,
+{
     fn get_component(&self, index: usize) -> Option<&'_ T> {
-        self.components.get(index)
+        match self.components.get(index) {
+            Some(Some(component)) => Some(component),
+            _ => None,
+        }
     }
 
     fn set_component(&mut self, index: usize, component: T) -> Result<(), ()> {
-        match self.components.len() {
-            len if index < len => {
-                self.components.get_mut(index) = Some(component);
-                Ok(())
-            }
-            len => {
-                self.components.reserve(index - len);
-                self.components.extend(repeat(None).take(index - len));
-                self.set_component(index, component)
-            }
+        if index >= self.components.len() {
+            self.resize(index + 1)?
         }
+        if let Some(stored) = self.components.get_mut(index) {
+            *stored = Some(component);
+        }
+        Ok(())
     }
 
     fn delete_component(&mut self, index: usize) -> Result<(), ()> {
