52f96e9dfb5a3a979a6f212ac7f94e75e291d7d0
Alex Barber <alex@dangerzonegames.com>
2023-08-18 18:53:08 -0600

    try a more simplified approach to start

--- src/lib.rs
+++ src/lib.rs
@@ -1,104 +1,84 @@
 //! An Entity Component System Library
+use std::fmt::Debug;
+use std::iter::repeat;
 
-use std::any::{Any, TypeId};
-use std::collections::{BTreeMap, HashMap};
+pub trait Component: Sized + Debug {}
 
-pub type VecComponentStorage<T: Sized> = Vec<Option<T>>;
-pub type HashMapComponentStorage<T: Sized> = HashMap<usize, T>;
-pub type BTreeMapComponentStorage<T: Sized> = BTreeMap<usize, T>;
-
-pub enum ComponentStorage<T: Sized> {
-    Table(VecComponentStorage<T>),
-    HashMap(HashMapComponentStorage<T>),
-    BTreeMap(BTreeMapComponentStorage<T>),
+pub trait ComponentStorage<T: Component> {
+    fn get_component(&self, index: usize) -> Option<&'_ T>;
+    fn set_component(&mut self, index: usize, component: T) -> Result<(), ()>;
+    fn delete_component(&mut self, index: usize) -> Result<(), ()>;
 }
 
-#[derive(Default)]
-pub struct Wyrd {
-    component_storages: HashMap<TypeId, *mut ()>,
-    entity_table: Vec<bool>,
+#[derive(Debug, Default)]
+pub struct ArrayComponentStorage<T: Component, const SIZE: usize> {
+    components: [Option<T>; SIZE],
 }
 
-impl Wyrd {
-    pub fn register_component_type<T: 'static>(&mut self) {
-        let mut data: VecComponentStorage<T> = VecComponentStorage::default();
-        self.component_storages
-            .insert(TypeId::of::<T>(), data.as_mut_ptr().cast::<()>());
+impl<T, const SIZE: usize> ComponentStorage<T> for ArrayComponentStorage<T, SIZE> where T: Component {
+    fn get_component(&self, index: usize) -> Option<&'_ T> {
+        self.components.get(index)
     }
 
-    // pub fn spawn_entity(&self, entity: BuiltEntity) {
-    //     for (type_id, component) in entity.builder.components.iter() {
-    //         if let Some(storage) = self.component_storages.get(type_id) {
-    //             let real_storage = unsafe { storage.cast::<ComponentStorage<type_id>>()}
-    //         }
-    //     }
-    // }
-
-    pub fn run_system(&self, component_types: &[TypeId], system: fn(Entity)) {
-        for (i, meta) in self.entity_table.iter().enumerate() {
-            if let true = meta {
-                system(Entity::new(self, i))
-            };
-        }
-    }
-
-    fn get_component_storage<T: 'static + Sized>(&self) -> Option<&ComponentStorage<T>> {
-        if let Some(pointer) = self.component_storages.get(&TypeId::of::<T>()) {
-            unsafe { return pointer.cast::<ComponentStorage<T>>().as_ref() }
-        }
-        None
-    }
-}
-
-#[derive(Default)]
-pub struct EntityBuilder {
-    components: HashMap<TypeId, Box<dyn Any>>,
-}
-
-impl EntityBuilder {
-    pub fn add_component<T: 'static>(&mut self, component: Box<T>) -> &mut Self {
-        self.components.insert(TypeId::of::<T>(), component);
-        self
-    }
-
-    pub fn build(self) -> EntityConfig {
-        EntityConfig { builder: self }
-    }
-}
-
-pub struct EntityConfig {
-    builder: EntityBuilder,
-}
-
-pub struct Entity<'a> {
-    wyrd: &'a Wyrd,
-    index: usize,
-}
-
-impl<'a> Entity<'a> {
-    fn new(wyrd: &'a Wyrd, index: usize) -> Self {
-        Self { wyrd, index }
-    }
-
-    pub fn has_component<T: 'static>(&self) -> bool {
-        matches!(self.get_component::<T>(), Some(_))
-    }
-
-    pub fn get_component<T: 'static>(&self) -> &'a Option<T> {
-        if let Some(pointer) = self.wyrd.component_storages.get(&TypeId::of::<T>()) {
-            unsafe {
-                if let Some(storage) = pointer.cast::<Vec<Option<T>>>().as_ref() {
-                    if let Some(component) = storage.get(self.index) {
-                        component
-                    } else {
-                        &None
-                    }
-                } else {
-                    &None
-                }
+    fn set_component(&mut self, index: usize, component: T) -> Result<(), ()> {
+        match index {
+            index if index < SIZE => {
+                self.components.get_mut(index) = Some(component);
+                Ok(())
             }
-        } else {
-            &None
+            _ => Err(())
         }
+    }
+
+    fn delete_component(&mut self, index: usize) -> Result<(), ()> {
+        match index {
+            index if index < SIZE => {
+                self.components.get_mut(index) = None;
+                Ok(())
+            }
+            _ => Err(())
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct VecComponentStorage<T: Component> {
+    components: Vec<Option<T>>,
+}
+
+impl<T> VecComponentStorage<T> where T: Component {
+    pub fn new_with_initial_size(initial_size: usize) -> Self {
+        let mut components = vec![];
+        components.reserve(initial_size);
+        Self {
+            components
+        }
+    }
+}
+
+impl<T> ComponentStorage<T> for VecComponentStorage<T> where T: Component {
+    fn get_component(&self, index: usize) -> Option<&'_ T> {
+        self.components.get(index)
+    }
+
+    fn set_component(&mut self, index: usize, component: T) -> Result<(), ()> {
+        match self.components.len() {
+            len if index < len => {
+                self.components.get_mut(index) = Some(component);
+                Ok(())
+            }
+            len => {
+                self.components.reserve(index - len);
+                self.components.extend(repeat(None).take(index - len));
+                self.set_component(index, component)
+            }
+        }
+    }
+
+    fn delete_component(&mut self, index: usize) -> Result<(), ()> {
+        if let Some(component) = self.components.get_mut(index) {
+            *component = None;
+        }
+        Ok(())
     }
 }
